2ffc51354826f7e37905c2c82ef27994dfcae27d
DESKTOP-M5KDF8G\chila <chila.granate@gmail.com>
2024-06-26 04:04:07 -0300

    Refactor: Extract SignUpBody composable function
    Extracted the body of the SignUpScreen into a separate composable function called SignUpBody for better code organization and
     reusability.

--- app/src/main/java/com/example/costsplitter/ui/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/costsplitter/ui/screens/SignUpScreen.kt
@@ -59,157 +59,155 @@ fun SignUpScreen(
         onNavigateToHome()
     }
 
-        Scaffold(
-            modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-            topBar = {
-                CostSplitterTopAppBar(
-                    title = stringResource(SignUpDestination.titleRes),
-                    canNavigateBack = true,
-                    navigateUp = onNavigateUp,
-                    scrollBehavior = scrollBehavior
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostSplitterTopAppBar(
+                title = stringResource(SignUpDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp,
+                scrollBehavior = scrollBehavior
+            )
+        },
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+
+            if (uiState.isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .align(Alignment.Center)
                 )
-            },
-        ) { innerPadding ->
-            Box(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize()
-            ) {
+            } else {
 
-                if (uiState.isLoading) {
-                    CircularProgressIndicator(
-                        modifier = Modifier
-                            .size(50.dp)
-                            .align(Alignment.Center)
-                    )
-                } else {
-
-                    SignUpBody(
-                        uiState,
-                        viewModel::onEmailChanged,
-                        viewModel::onPasswordChanged,
-                        viewModel::onConfirmPasswordChanged,
-                        viewModel::onPhoneNumberChanged,
-                        viewModel::onTogglePasswordVisibility,
-                        viewModel::onToggleConfirmPasswordVisibility,
-                        viewModel::registerUser,
-                    )
-                }
+                SignUpBody(
+                    uiState,
+                    viewModel::onEmailChanged,
+                    viewModel::onPasswordChanged,
+                    viewModel::onConfirmPasswordChanged,
+                    viewModel::onPhoneNumberChanged,
+                    viewModel::onTogglePasswordVisibility,
+                    viewModel::onToggleConfirmPasswordVisibility,
+                    viewModel::registerUser,
+                )
             }
         }
     }
+}
 
 
-    //
-    @Composable
-    fun SignUpBody(
-        modifier: Modifier = Modifier,
-        signUpUiState: SignUpUiState,
-        onEmailChanged: (String) -> Unit,
-        onPasswordChanged: (String) -> Unit,
-        onConfirmPasswordChanged: (String) -> Unit,
-        onPhoneNumberChanged: (String) -> Unit,
-        onTogglePasswordVisibility: () -> Unit,
-        onToggleConfirmPasswordVisibility: () -> Unit,
-        onSignUpClicked: () -> Unit = {},
+@Composable
+fun SignUpBody(
+    signUpUiState: SignUpUiState,
+    onEmailChanged: (String) -> Unit,
+    onPasswordChanged: (String) -> Unit,
+    onConfirmPasswordChanged: (String) -> Unit,
+    onPhoneNumberChanged: (String) -> Unit,
+    onTogglePasswordVisibility: () -> Unit,
+    onToggleConfirmPasswordVisibility: () -> Unit,
+    onSignUpClicked: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
 
-    ) {
-
-        Column(modifier = modifier.padding(16.dp)) {
-            OutlinedTextField(
-                value = signUpUiState.email,
-                onValueChange = onEmailChanged,
-                label = { Text("Email") },
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            PasswordField(
-                password = signUpUiState.password,
-                label = "Password",
-                onPasswordChanged = onPasswordChanged,
-                onTogglePasswordVisibility = onTogglePasswordVisibility,
-                isPasswordVisible = signUpUiState.isPasswordVisible,
-            )
-
-            PasswordField(
-                password = signUpUiState.confirmPassword,
-                label = "Confirm Password",
-                onPasswordChanged = onConfirmPasswordChanged,
-                onTogglePasswordVisibility = onToggleConfirmPasswordVisibility,
-                isPasswordVisible = signUpUiState.isConfirmPasswordVisible,
-            )
-
-
-
-            OutlinedTextField(
-                value = signUpUiState.phone,
-                onValueChange = onPhoneNumberChanged,
-                label = { Text("Phone Number") },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Phone,
-                    imeAction = ImeAction.Done
-                ),
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            Button(
-                onClick = onSignUpClicked,
-                enabled = signUpUiState.isValid,
-                modifier = Modifier
-                    .align(Alignment.End)
-                    .fillMaxWidth()
-                    .padding(top = 16.dp)
-            ) {
-                Text(text = "Sign Up")
-            }
-
-        }
-    }
-
-
-    @Composable
-    fun PasswordField(
-        password: String,
-        onPasswordChanged: (String) -> Unit,
-        isPasswordVisible: Boolean,
-        onTogglePasswordVisibility: () -> Unit,
-        label: String,
-        modifier: Modifier = Modifier
-    ) {
+    Column(modifier = modifier.padding(16.dp)) {
         OutlinedTextField(
-            value = password,
-            onValueChange = onPasswordChanged,
-            label = { Text(label) },
-            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-            trailingIcon = {
-                IconButton(
-                    onClick = { onTogglePasswordVisibility() }
-                ) {
-                    val image = painterResource(
-                        if (isPasswordVisible) R.drawable.ic_visibility_off else R.drawable.ic_visibility
-                    )
+            value = signUpUiState.email,
+            onValueChange = onEmailChanged,
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
 
-                    Icon(
-                        painter = image,
-                        contentDescription = if (isPasswordVisible) "Hide password" else "Show password"
-                    )
-                }
-            },
-            singleLine = true,
+        PasswordField(
+            password = signUpUiState.password,
+            label = "Password",
+            onPasswordChanged = onPasswordChanged,
+            onTogglePasswordVisibility = onTogglePasswordVisibility,
+            isPasswordVisible = signUpUiState.isPasswordVisible,
+        )
+
+        PasswordField(
+            password = signUpUiState.confirmPassword,
+            label = "Confirm Password",
+            onPasswordChanged = onConfirmPasswordChanged,
+            onTogglePasswordVisibility = onToggleConfirmPasswordVisibility,
+            isPasswordVisible = signUpUiState.isConfirmPasswordVisible,
+        )
+
+
+
+        OutlinedTextField(
+            value = signUpUiState.phone,
+            onValueChange = onPhoneNumberChanged,
+            label = { Text("Phone Number") },
             keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password,
+                keyboardType = KeyboardType.Phone,
                 imeAction = ImeAction.Done
             ),
-            modifier = modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth()
         )
+
+        Button(
+            onClick = onSignUpClicked,
+            enabled = signUpUiState.isValid,
+            modifier = Modifier
+                .align(Alignment.End)
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            Text(text = "Sign Up")
+        }
+
     }
+}
 
 
-    @Preview
-    @Composable
-    fun SignUpScreenPreview() {
-        SignUpScreen(
-            navigateBack = {},
-            onNavigateUp = {},
-            onNavigateToHome = {})
-    }
+@Composable
+fun PasswordField(
+    password: String,
+    onPasswordChanged: (String) -> Unit,
+    isPasswordVisible: Boolean,
+    onTogglePasswordVisibility: () -> Unit,
+    label: String,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = password,
+        onValueChange = onPasswordChanged,
+        label = { Text(label) },
+        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        trailingIcon = {
+            IconButton(
+                onClick = { onTogglePasswordVisibility() }
+            ) {
+                val image = painterResource(
+                    if (isPasswordVisible) R.drawable.ic_visibility_off else R.drawable.ic_visibility
+                )
+
+                Icon(
+                    painter = image,
+                    contentDescription = if (isPasswordVisible) "Hide password" else "Show password"
+                )
+            }
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Done
+        ),
+        modifier = modifier.fillMaxWidth()
+    )
+}
+
+
+@Preview
+@Composable
+fun SignUpScreenPreview() {
+    SignUpScreen(
+        navigateBack = {},
+        onNavigateUp = {},
+        onNavigateToHome = {})
+}
